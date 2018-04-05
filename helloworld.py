f2085638a28b7d61630f3d443c1dc27ed3b6ef9f
abhishek-karmakar <abhishek.karmakar10@gmail.com>
2018-04-05 19:40:03 +0530

    functions

--- helloworld.py
+++ helloworld.py
@@ -2,46 +2,19 @@ import random
 import sys
 import os
 
-# for looping 
+# functions : Reuse the code. 
 
-for x in range(0,10):
-	print(x)
+def addnumber(fnum, lnum):
+	snum = fnum + lnum
+	return snum
 
-print('\n')
-
-grocerry_list = ['Juice', 'Tomatoes', 'Potatoes', 'Bananas']
-
-for y in grocerry_list:
-	print(y)
-
-for x in [2,4,6,8,10]:
-	print (x)
-
-num_list = [[1,2,3],[10,20,30],[100,200,300]]
-
-for x in range(0,3):
-	for y in range(0,3):
-		print(num_list[x][y])
+print(addnumber(1,4))
 
 
+def getinputfromuser():
+	print("Please enter your name")
+	name = sys.stdin.readline()
+	print ('Helo' , name)
 
-# While Loop : When we do not know about the range
+getinputfromuser()
 
-random_num = random.randrange(0,100)
-
-while(random_num != 15):
-	print(random_num)
-	random_num = random.randrange(0,100)
-
-i=0;
-
-while(i <= 20 ):
-	if(i%2 ==0):
-		print(i)
-	elif(i==9):
-		break
-	else:
-		i +=1
-		continue #this will start the loop frm the begenenning
-
-	i += 1
