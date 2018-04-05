8ca1c15f7e8c1a0b12dd982cd275f6e44fd1d5f0
abhishek-karmakar <abhishek.karmakar10@gmail.com>
2018-04-05 19:22:07 +0530

    Conditional Operators

--- helloworld.py
+++ helloworld.py
@@ -2,25 +2,32 @@ import random
 import sys
 import os
 
-#Dictiontionaries and maps
-''' cannot join Disctionaries like we can join a list. '''
+#conditionals if else elif == != > >= <=
+# logic gates are like (and or not) 
 
-hp_chars = {'Harry':'Potter',
-				'Ron':'Wisely',
-				'Hermoine':'Granger',
-				'Albus':'Dumbledore'}
+age = 21
 
-print(hp_chars['Harry'])
-del hp_chars['Albus']
-hp_chars['Albus'] = 'Percivic'
+if age > 16:
+	print("You are old enought to drive")
+else:
+	print("Not old enough to drive")
 
-print (len(hp_chars))
+if age >= 21:
+	print('You are old enough to drive a tractor trailer')
+elif age >=16:
+	print("You are old enoug to drive a car")
+else:
+	print("You are not old enough to drive")
 
-print (hp_chars.get("Harry"))
+if((age >= 1) and (age <=18)):
+	print("You get a birthday")
+elif(age == 21) or (age >= 65):
+	print("You get a birthday")
+elif not(age == 30):
+	print("You do not get your birthday")
+else:
+	print("You get a birthday party .. yeah")
 
-print(hp_chars.keys())
 
-print(hp_chars.values())
-	
 
 
