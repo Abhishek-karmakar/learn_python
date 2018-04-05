f94aa7824c6445da1ac98985b11e4afa92d2e787
abhishek-karmakar <abhishek.karmakar10@gmail.com>
2018-04-05 18:20:20 +0530

    checking what all the operators do

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,28 @@ import sys
 import os
 
 print("Hello World")
+
+#commen
+
+'''
+multiline comment
+comes here
+
+Numbers Strings List Tuples 
++-* / % 
+'''
+name = "Derek"
+print (name)
+
+#checking what all the numerals do. 
+
+print("5+2 = "5+2 )
+print("5-2 = "5-2 )
+print("5*2 = "5*2 )
+print("5%2 = "5%2 )
+print("5**2 = "5**2 )
+print("5//2 = "5//2 )
+
+
+
+
