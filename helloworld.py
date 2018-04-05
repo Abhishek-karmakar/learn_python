b2d247f97e54c1b646601e0cbf5515ba39eb17ef
abhishek-karmakar <abhishek.karmakar10@gmail.com>
2018-04-05 18:40:18 +0530

    Different types of prints

--- helloworld.py
+++ helloworld.py
@@ -18,13 +18,35 @@ print (name)
 
 #checking what all the numerals do. 
 
-print("5+2 = "5+2 )
-print("5-2 = "5-2 )
-print("5*2 = "5*2 )
-print("5%2 = "5%2 )
-print("5**2 = "5**2 )
-print("5//2 = "5//2 )
+print("5+2 = ",5+2 )
+print("5-2 = ",5-2 )
+print("5*2 = ",5*2 )
+print("5%2 = ",5%2 )
+print("5**2 = ",5**2 )
+print("5//2 = ",5//2 )
 
 
+#order of operation matters. 
+print("1 + 2 - 3 * 2 =",)
 
+
+#quote
+quote = "\" life is a lie, enjoy the pie \""
+
+multiline_quote = ''' Multiline is next line
+This is the next line'''
+
+new_string = quote + multiline_quote
+print(new_string)
+
+# 2 different type of srring
+print("%s %s %s" % ('I like the quote', quote, multiline_quote))
+
+# to print 5 new lines
+print('\n' * 5)
+
+'''
+print("I don't like", end="")
+print("newlines")
+'''
 
